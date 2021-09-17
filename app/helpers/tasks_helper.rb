af19b474f203467be8b4bc828bfeb794d636379f
BylevskijSV <sergeibylewskii@gmail.com>
2021-09-17 18:39:57 +0300

    fix key 'in progress'

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,9 +1,9 @@
 module TasksHelper
   def status_color(status)
     {
-      new: 'danger',
-      in_progress: 'warning',
-      done: 'success'
+      'new': 'danger',
+      'in progress': 'warning',
+      'done': 'success'
     }[status.to_sym]
   end
 end
