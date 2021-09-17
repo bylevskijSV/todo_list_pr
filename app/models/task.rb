6d2dc866f4f7f6eef933aa8264d8493fcf3f6063
BylevskijSV <sergeibylewskii@gmail.com>
2021-09-17 16:19:42 +0300

    implemented the complete method

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,5 +4,13 @@ class Task < ApplicationRecord
   validates :description, presence: true, length: { minimum: 10, maximum: 300 }
   validates :title, presence: true, length: { minimum: 3, maximum: 100 }
 
-  
+  STATUSES = [
+    NEW = 'new',
+    IN_PROGRESS = 'in progress',
+    DONE = 'done'
+  ]
+
+  def complete!
+    update_column(:status, Task::DONE)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "tasks#index"
-  resources :tasks
+  resources :tasks do
+    member do
+      get 'complete'
+    end
+  end
 end
